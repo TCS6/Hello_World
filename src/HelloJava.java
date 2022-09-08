eb84105cdcd9c65f6d47342a645a756e2b70e147
MILU <2841234610@qq.com>
2022-09-08 23:13:26 +0800

    Initial commit

---
+++ src/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava
+{
+    public static void main(String[] args)
+    {
+        String helloJava = "Hello Java!";//定义字符串(变量采用驼峰命名法)并赋值为Hello Java!
+        System.out.println(helloJava);//控制台输出变量helloJava
+        System.out.println("姓名:陆际开\n性别:男\n籍贯:云南昆明\n住址:宜良县");//\n用于换行
+    }
+}
